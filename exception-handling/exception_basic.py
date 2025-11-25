2a4403c87d58e82ecc97c96c5feeb6cc335df172
Torikul Islam Oli <torikulislamoli@Torikuls-MacBook-Air.local>
2025-11-25 22:26:53 +0100

    exception handling 01

---
+++ exception-handling/exception_basic.py
@@ -0,0 +1,11 @@
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+
+c = a // b
+print(c) # this throw a NameError: name d is not found
+
+
+# if c == 10 # this will throw a 'SyntaxError: invalid syntax'
+#     print(c) # this will throw a 'IndentationError: unexpected indent'
+    
+    
---
+++ exception-handling/logical_error.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input('Enter the second number: '))
+c = int(input('Enter the third number: '))
+
+d = a // b - c  # this will give a differnt resutl due to the missing brackets.
+
+
+print(d)
